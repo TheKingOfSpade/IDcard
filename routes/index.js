52cfd8df48bdc42c0530ec5c12e230d38b80b4ad
aw9p <aw9p@fresnou.org>
2023-09-18 09:29:07 -0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index');
+});
+module.exports = router;
+router.post('/card', function (req, res){
+  console.log(req.body.dateOfBirth)
+  res.render('card', {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    type: req.body.type,
+    dateOfBirth: new Date(req.body.dateOfBirth),
+    addressLine1: req.body.addressLine1,
+    addressLine2: req.body.addressLine2,
+    city: req.body.city,
+    state: req.body.state,
+    zip: req.body.zip,
+    accountNumber: 12345,
+    currentDate: new Date()
+  });
+})
